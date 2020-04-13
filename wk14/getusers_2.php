4d5484c4ed24b5f36e22e97ae9103ffee44f7068
unknown <quangminhtran135@gmail.com>
2020-04-13 01:17:20 -0400

    final

---
+++ wk14/getusers_2.php
@@ -0,0 +1,58 @@
+<form action="getusers_2.php" method='GET'>
+  <label for="fname">First name:</label>
+  <input type="text" id="fname" name="fname"><br>
+  <input type="submit" value="Submit">
+</form>
+<?php
+$servername = "localhost";
+$username = "minhtran";
+$dbname = "cyber";
+$password = "vebragax123";
+$conn = new mysqli($servername, $username, $password, $dbname);
+if($conn->connect_error){
+   die("Connection failed: " . $conn->connect_error);
+}
+
+// Get data
+
+if(isset($_GET['fname'])){
+ $fname = $_GET['fname'];
+
+$active=1;
+$stmt = $conn->prepare("SELECT * FROM users WHERE firstname=? AND active=?");
+$stmt->bind_param("si", $fname,$active);
+$stmt->execute();
+
+// Table
+
+echo "<table>";
+echo "<tr>";
+echo "<th>ID</th>";
+echo "<th>Username</th>";
+echo "<th>Email</th>";
+echo "<th>First Name</th>";
+echo "<th>Last Name</th>";
+echo "<th>Active</th>";
+echo "</tr>";
+if($result->num_rows > 0) {
+while($row = $result->fetch_assoc()) {
+    echo "<tr>";
+    echo "<td>".$row['id']."</td>";
+    echo "<td>".$row['username']."</td>";
+    echo "<td>".$row['email']."</td>";
+    echo "<td>".$row['firstname']."</td>";
+    echo "<td>".$row['lastname']."</td>";
+    echo "<td>".$row['active']."</td>";
+    echo "</tr>";
+}
+}
+else {
+  echo "None found";
+}
+$conn->close();
+
+}
+else {
+ $fname = "";
+}
+?>
